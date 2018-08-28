462072cd4993b8936b495a98bd7aad348ece76b8
Miriam Zusin <1rosehip@gmail.com>
2018-08-28 09:45:29 +0300

    lesson 6

--- lesson-4/main.js
+++ lesson-4/main.js
@@ -1,5 +1,4 @@
 (() => {
-    'use strict';
 
     console.log('test');
     console.log(document);
--- lesson-6/main.js
+++ lesson-6/main.js
@@ -1,5 +1,4 @@
 (() => {
-    'use strict';
 
     const cat = document.getElementById('my-cat');
 
